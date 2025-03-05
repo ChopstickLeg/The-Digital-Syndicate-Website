4d9668a1f8b3b1194b983dbd86799d77771bbbb0
ChopstickLeg <carpenterhayden1010@gmail.com>
2025-03-05 07:39:15 -0500

    Tried to work on helmet implementation

--- web/src/pages/socials.tsx
+++ web/src/pages/socials.tsx
@@ -1,5 +1,14 @@
+import { Helmet } from "react-helmet-async";
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <>
+      <Helmet>
+        <title>Socials</title>
+        <meta name="description" content="Digital Syndicate Socials" />
+      </Helmet>
+      <label>Socials</label>
+    </>
+  );
 };
 
 export default Socials;
